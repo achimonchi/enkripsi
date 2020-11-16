c05b7aa044fa22cdfa6816eaf33d02e32db70f26
achimonchi <achimonchi.reyhan@gmail.com>
2020-11-16 12:07:48 +0700

    set fontawesome

--- src/components/Algorithm.svelte
+++ src/components/Algorithm.svelte
@@ -1,22 +1,21 @@
 <script>
     import Card from './../shared/Card.svelte';
     import Affine from './Affine.svelte';
-    import Hill from './Hill.svelte';
     import Vigenere from './Vigenere.svelte';
     
     const algorithms = [
         {name : "Affine Cipher", url : "affine-cipher", component:Affine},
         {name : "Vigenere Cipher", url : "vigenere-cipher", component:Vigenere},
-        {name : "Hill Cipher", url : "hill-cipher", component:Hill},
+        // {name : "Hill Cipher", url : "hill-cipher", component:Hill},
     ];
-    let algoActive = {name : "Vigenere Cipher", url : "vigenere-cipher", component:Vigenere}
+    let algoActive = {}
     
     const algoChange=(e)=>{
         algoActive = e.detail
     }
 </script>
 
-<section id="algo">
+<section id="algo" class="mb-5">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -24,7 +23,7 @@
                     <h4 class="mb-3" style="font-weight:400">Pilih Algoritma Enkripsi</h4>
                     <div class="row">
                         {#each algorithms as algo}
-                            <div class="col-md">
+                            <div class="col-md mb-3">
                                 <Card on:algoChange={algoChange} algoActive={algoActive} algo={algo}/>
                             </div>
                         {/each}
--- src/shared/Card.svelte
+++ src/shared/Card.svelte
@@ -9,21 +9,33 @@
 
 
 <div on:click={()=>dispatch('algoChange', algo)} class="card" class:active={algoActive.name === algo.name}>
-    <h4 class="text-center">{algo.name}</h4>
+    <h4>{algo.name}</h4>
+    <h4><i class="fas fa-check-circle" style="color:white"></i></h4>
 </div>
 
 
 <style>
 
     .card.active{
-        background: red;
+        background: #30475e;
         color:white;
+    }
+    h4{
+        padding: 0;
+        margin: 0;
+    }
+    i{
+        float: right;
+        align-self: center;
     }
     .card{
         border:none;
         box-shadow: 0px 5px 10px -5px rgba(0,0,0,.1);
         padding:20px;
         transition: .3s;
+        display: flex !important;
+        flex-direction: row;
+        justify-content: space-between;
     }
 
     .card:hover{
