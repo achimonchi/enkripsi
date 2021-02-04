ed8d5b15cafff945b14d144b107f67c88aa80703
achimonchi <achimonchi.reyhan@gmail.com>
2021-02-04 17:46:37 +0700

    generate public and private key

--- src/logicSample/kunciPublik/RSA.js
+++ src/logicSample/kunciPublik/RSA.js
@@ -5,29 +5,104 @@ const n = p*q;
 
 const toutientN = (p-1)*(q-1);
 
-const arrRelatifPrima = [];
+const arrRelatifPrima = getRelativePrime(toutientN);
+const e = arrRelatifPrima[18];
 
-if(isPrime(toutientN)){
-    
-} else {
-    for(let i=1; i<toutientN; i++){
-        if(toutientN % 2 !== 0){
-            
-        } else {
-            if(toutientN % i !== 0){
-                if(isPrime(i)){
-                    
+function getD(){
+    let i = 1;
+    let flag = true;
+    while(flag){
+        if(mod(e*i, toutientN) === 1){
+            console.log(i)
+            flag = false;
+        }
+        i++;
+        // console.log(mod(21,20));
+    }
+}
+
+getD();
+
+
+function getRelativePrime(toutientN){
+    let relativePrime = [];
+    if(isPrime(toutientN)){
+        for(let i = 1; i<toutientN - 1; i++){
+            relativePrime.push(i);
+        }
+    } else {
+        if(toutientN % 2 === 0 ){
+            for(let i = 1; i<toutientN; i++){
+                if(i == 1){
+                    relativePrime.push(i);
+                }else if(toutientN % i !== 0){
+                    const prime = generatePrime(i);
+                    let temp = true;
+                    for(let j = 0; j< prime.length; j++){
+                        if(i % prime[j] === 0){
+                            temp = false;
+                            break;
+                        }
+                    }
+                    if(temp === true){
+                        if(relativePrime.length < 20){
+                            relativePrime.push(i);
+                        } else {
+                            break;
+                        }
+                    }
                 }
             }
         }
-    };
+        else {
+            for(let i=1; i<toutientN; i++){
+                if( i === 1){
+                    relativePrime.push(i);
+                } else {
+                    if(toutientN % i !== 0){
+                        const prime = generatePrime(i);
+                        let temp = true;
+                        for(let j = 0; j< prime.length; j++){
+                            if(i % prime[j] === 0){
+                                temp = false;
+                                break;
+                            }
+                        }
+                        if(temp === true){
+                            if(relativePrime.length < 20){
+                                relativePrime.push(i);
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+                
+            };
+        }
+    }
+    return relativePrime;
 }
 
+function mod(x, m=0){
+    const pow = m==0? Math.pow(2,32) : m;
+    return (x % pow + pow) % pow
+}
+
+function generatePrime(maxNumber){
+    let prime=[];
+    for(let i=2; i<maxNumber; i++){
+        if(isPrime(i)){
+            prime.push(i);
+        }
+    }
+    return prime;
+}
 
 
 function isPrime(number){
     let count =0;
-    for(let i=2; i<number; i++){
+    for(let i=1; i<number; i++){
         if(number%i === 0)
             count++;
     }
